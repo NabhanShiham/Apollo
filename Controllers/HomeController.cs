cbdeae766fb4e65db59276b245de1a881f0ded6e
NabhanShiham <nabhanshiham@gmail.com>
2025-12-24 22:20:55 +0500

    work: dashboard shows some count of books available and loaned

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,20 +1,37 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Apollo.Models;
+using Apollo.Data;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Apollo.Entities;
 
 namespace Apollo.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly ApplicationDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
+        string? userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        IEnumerable<Book> books = await _context.Books
+            .Where(b => b.OwnerId == userId)
+            .OrderByDescending(b => b.CreatedAt)
+            .ToListAsync();
+
+        int countOfAvailableBooks = books.Count(b => b.IsBorrowed != true);
+
+        ViewBag.countOfAvailableBooks = countOfAvailableBooks;
+        ViewBag.countOfLoanedBooks = books.Count(b => b.IsBorrowed == true);
         return View();
     }
 
